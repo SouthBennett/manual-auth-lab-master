2e5b5330318822ccb5c717ae5a650b21c748f6ce
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-03-04 19:41:28 -0800

    Complete: Manual Authentication Lab

--- src/app.js
+++ src/app.js
@@ -1,8 +1,31 @@
 import express from 'express';
 import defaultRouter from './routers/routes.js';
+import session from 'express-session';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 //configure Express.js app
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+// enable first session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+// Attach user to every request second
+app.use((req, res, next) => {
+  if (req.session.user) {
+    req.user = req.session.user;
+  } else {
+    req.user = null;
+  }
+  next();
+});
 
 //view engine
 app.set("view engine", "ejs");
